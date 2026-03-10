40e2a844f53e5bdaab5033ada08b2841e26ff130
Najma101 <najma.aaden11@gmail.com>
2026-03-10 15:41:20 +0000

    initial commit

---
+++ ch1_objects/script.js
@@ -0,0 +1,47 @@
+// let firstName = "Ali";
+// let lastName = "Mohammed";
+// let age = 30;
+
+// let firstName = "Fadumo";
+// let lastName = "Nuur";
+// let age = 30;
+
+// let firstName = "Najma";
+// let lastName = "Aden";
+// let age = 30;
+
+// let person1 = {
+//   firstName: "Ali",
+//   lastName: "Mohammed",
+//   age: 30,
+// };
+
+// let person2 = {
+//   firstName: "Fadumo",
+//   lastName: "Nuur",
+//   age: 30,
+// };
+
+// let person3 = {
+//   firstName: "Najma",
+//   lastName: "Aden",
+//   age: 30,
+// };
+// person1.firstName = "Ahmed";
+
+// delete person1.lastName;
+
+// console.log(person1["firstName"]);
+// console.log(Math.PI)
+// console.log(Math.round(4.6)); // 5
+// console.log(Math.round(4.4)); // 4
+
+// console.log(Math.ceil(4.1)); // 4
+
+let today = new Date("2025-01-01");
+console.log(today.getFullYear());
+
+console.log(today.getMonth());
+
+
+
